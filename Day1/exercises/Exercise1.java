5429357a9f1b687ec7748ead69e44d1d7dea0a5d
Ashok Budha <abudha592@gmail.com>
2024-12-13 20:28:31 +0545

    Adding exercise1 of Day1

---
+++ Day1/exercises/Exercise1.java
@@ -0,0 +1,7 @@
+// Exercise1.java
+public class Exercise1 {
+    public static void main(String[] args) {
+        // Print "Hello World" to the console
+        System.out.println("Hello World");
+    }
+}
