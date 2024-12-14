cfc2f852f2ec4cf435a24c3c65975b8064d4ba1f
Ashok Budha <abudha592@gmail.com>
2024-12-14 06:35:51 +0545

    Added Exercise1 to Day2

---
+++ Day2/exercises/Exercise1.java
@@ -0,0 +1,16 @@
+// Write a basic Java program with a main method that prints the sum of two numbers.
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 5;
+        int num2 = 7;
+
+        // Calculate the sum of the two numbers
+        int sum = num1 + num2;
+
+        // Print the sum
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
